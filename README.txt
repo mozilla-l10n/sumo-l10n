ad292f5fd7ce6d7a35cfb2c558380dded2229315
Staś Małolepszy <stas@mozilla.com>
2010-05-10 17:23:55 +0000

    Bug 564831: Add translations from TikiWiki's DB in form on compendium PO files

--- README.txt
+++ README.txt
@@ -1,12 +1,17 @@
 We are using English strings for gettext message ids.
 
 Instructions:
-1) Run ./manage.py extract in the kitsune directory. This will create kitsune/locale/z-messages.pot
-2) merge-po.sh z-messages.pot en_US
-3) merge-enus2all.sh
+1) Run 
+     ./manage.py extract 
+   in the kitsune directory. This will create kitsune/locale/z-messages.pot
+2) cd kitsune/locale
+3) cp z-messages.pot templates/LC_MESSAGES/messages.pot
+   Putting the POT file in templates/ will allow Verbatim to merge the PO files with it automatically.
+4) ./merge-po.sh templates/LC_MESSAGES/messages.pot .
+
 
 Optional:
-4) compile-mo.sh .
+5) compile-mo.sh .
 
 New Locales:
-msginit --locale=fr -i en_US/LC_MESSAGES/messages.po
+msginit --locale=fr -i templates/LC_MESSAGES/messages.pot
--- merge-po.sh
+++ merge-po.sh
@@ -16,17 +16,16 @@ echo $TMPFILE
 # check if file and dir are there
 if [[ ($# -ne 2) || (! -f "$1") || (! -d "$2") ]]; then usage; fi
 
-for lang in `find $2 -type f -name "*.po"`; do
-    echo "Lang: ${lang}"
-    # this line was changed because msgmerge was outputting nothing
-    # and this nothingness was being written into en_US/LC_MESSAGES/messages.po
-    # I *think* this was written under the assumption that msgmerge
-    # actually outputted the results of the merge to stdin, but this is not
-    # the case for msgmerge (GNU gettext-tools) 0.17
-    # old lines: 
-    # msgmerge --update $lang $1 > $TMPFILE
-    # cp "$TMPFILE" "$lang"
-    msgmerge --update $lang $1
+for lang in `find $2 -type f -name "*.po" | grep -v templates`; do
+    echo -n "Lang: ${lang}"
+    tiki_lang=`dirname $lang | sed -e 's/\/LC_MESSAGES//'`
+    if [ -f tiki/${tiki_lang}.compendium ]; then
+        echo -n " (using legacy Tiki translations) "
+        msgmerge -C tiki/${tiki_lang}.compendium --update $lang $1
+    else
+        msgmerge --update $lang $1
+    fi
+    
 done
 rm "$TMPFILE"
 
